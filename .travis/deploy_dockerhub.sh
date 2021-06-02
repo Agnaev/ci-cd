58880deea458c0eaa302ecea24280463841576f6
Agnaev <Black8192water@gmail.com>
2021-06-02 15:36:40 +0300

    change docker deployment file

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -6,5 +6,6 @@ else
     TAG="$TRAVIS_BRANCH"
 fi
 REPO=$(echo "$TRAVIS_REPO_SLUG" | tr '[:upper:]' '[:lower:]')
+echo "$REPO"
 docker build -f Dockerfile -t "$REPO":"$TAG" .
 docker push "$TRAVIS_REPO_SLUG"
